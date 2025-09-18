7db3e2d445f9773d3deda8acd1a92fa5ba9913da
YM-phaseone <phaseone6741e@gmail.com>
2025-09-18 15:07:09 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let price = Math.floor(Math.random() * (350 - 250 + 1)) + 250;
+
+console.log(`りんごの値段が${price}円でした！`);
+
+if (price <= 300) {
+    console.log('まさる君はりんごを買いました！');
+} else {
+    console.log('まさる君はりんごを買いませんでした！');
+}
